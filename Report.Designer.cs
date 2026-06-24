32622996d353b36a61096cf3ca50cfaace2b909d
Fana06 <aquillafazanaufan@gmail.com>
2026-06-24 19:30:13 +0700

    membuat form report

---
+++ Report.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace BookingKontrolPasien.Forms
+{
+    partial class Report
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ShowCloseButton = false;
+            this.crystalReportViewer1.ShowGroupTreeButton = false;
+            this.crystalReportViewer1.ShowLogo = false;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1000, 650);
+            this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // Report
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 650);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Name = "Report";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Laporan Dokter - Crystal Report";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Report_FormClosed);
+            this.Load += new System.EventHandler(this.Report_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+    }
+}
---
+++ Report.cs
@@ -0,0 +1,95 @@
+﻿using BookingKontrolPasien.Helpers;
+using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BookingKontrolPasien.Forms
+{
+    public partial class Report : Form
+    {
+        private ReportDocument _reportDocument;
+
+        public Report()
+        {
+            InitializeComponent();
+        }
+
+        private void Report_Load(object sender, EventArgs e)
+        {
+            LoadLaporanDokter();
+        }
+
+        private void LoadLaporanDokter()
+        {
+            try
+            {
+                string reportPath =
+                Path.Combine(
+                Application.StartupPath,
+                "LaporanDokter.rpt");
+
+                if (!File.Exists(reportPath))
+                {
+                    MessageBox.Show(
+                        "File report tidak ditemukan.\n\n" +
+                        reportPath);
+
+                    return;
+                }
+
+                // Ambil data dokter dari database
+                DataTable dt = DBHelper.ExecuteQuery(@"
+                    SELECT
+                        CAST(id AS varchar(10)) AS id,
+                        nama_dokter,
+                        spesialisasi,
+                        no_hp,
+                        CASE
+                            WHEN status_aktif = 1 THEN 'Aktif'
+                            ELSE 'Nonaktif'
+                        END AS status_aktif
+                    FROM dokter
+                    ORDER BY id
+                ");
+
+                // Isi dataset yang dipakai Crystal Report
+                DSDokter ds = new DSDokter();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    ds.Tables["Dokter"].ImportRow(row);
+                }
+
+                _reportDocument = new ReportDocument();
+
+                _reportDocument.Load(reportPath);
+
+                _reportDocument.SetDataSource(ds);
+
+                crystalReportViewer1.ReportSource = _reportDocument;
+
+                crystalReportViewer1.RefreshReport();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    "Crystal Report",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void Report_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (_reportDocument != null)
+            {
+                _reportDocument.Close();
+                _reportDocument.Dispose();
+                _reportDocument = null;
+            }
+        }
+    }
+}
